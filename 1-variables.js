b0953cee620e6dee55e19bb226d65ec39eba3256
santiago rojas <santiago_bogota@hotmail.com>
2023-04-11 17:29:37 -0500

    cree dos variables nuevas

--- 1-variables.js
+++ 1-variables.js
@@ -2,12 +2,18 @@
 Para declarar una variable, se puede hacer uso de las palabras reservadas var o let como se ve a continuación.
 La sintaxis que se usará para declarar una variable será:
     var / let nombreVariable = valorVariable.
-Para ver el valor que se almacena en la variables en JavaScript se puede utilizar el método console.log(nombreVariable) como se ve a continuación*/
+Para ver el valor que se almacena en la variables en JavaScript se puede utilizar el método console.log(nombreVariable) como se ve a continuación
 
 var hola="Hola mundo" 
 console.log(hola)
 
-/*Las variables pueden cambiar de valor en el transcurso del código cómo se ve a continuación*/
+/*Las variables pueden cambiar de valor en el transcurso del código cómo se ve a continuación
 
 var hola="Hola mundo! x2"
 console.log(hola)
+*/
+
+let nombre= "santiago";
+let edad = 26;
+
+console.log(nombre,edad);
