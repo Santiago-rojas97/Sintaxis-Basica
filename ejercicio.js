e3efa1f4fcb42464126a058318776dad3f30e708
santiago rojas <santiago_bogota@hotmail.com>
2023-04-12 17:57:37 -0500

    opeadores matematicos

---
+++ ejercicio.js
@@ -0,0 +1,11 @@
+let x=10;
+let y=5;
+let z=18;
+
+let op1= (x*y)/z;
+let op2= (y+z)*x;
+let op3= (x/y)-(x-z);
+
+console.log(`el resulultado de la operacion es ${op1}`);
+console.log(`el resulultado de la operacion es ${op2}`);
+console.log(`el resulultado de la operacion es ${op3}`);
