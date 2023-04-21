968cbacb43e58e848bde4d6d2b5248b8070dc0f3
santiago rojas <santiago_bogota@hotmail.com>
2023-04-21 16:19:05 -0500

    Ejercicios con clases y subclse,herencia,poli y encapsulamiento

---
+++ avanzado.js
@@ -0,0 +1,9 @@
+
+try {
+    console.log('Inicio');
+    let suma=15+99;
+    console.log(`El resultado es ${resultado}`);
+    //Variable resultado no definida
+} catch (err) {
+    console.log('Hay un error!');
+}
--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+/*
 class Animal{
     constructor (nombre,edad){
     this.nombre = nombre;
@@ -8,6 +9,7 @@ class Animal{
     saludar(){
         return `Hola soy un animal y me llamo ${this.nombre}`
     }
+    
 
     
 }
@@ -39,26 +41,29 @@ console.log(gato.saludar());
 let cow = new Vaca("Lola",10,"lechera");
 console.log(cow.nombre,cow.saludar(),cow.tipo)
 
+*/
 
 class Figura{
     constructor(color){
         this.color = color;
     }
-    saludar(){
+    saludar(){//Metodo
         return `Hola! Soy una figura y mi color es ${this.color}`
     }
+    area(){
+
+    }
+    
 }
 
-class cuadradp extends Figura{
-    constructor(color,alto,peso){
-        super(color,alto=1.77,peso=65);
+
+class Cuadradp extends Figura{//Creando una Subclase
+    constructor(color,alto,ancho){
+        super(color,alto,ancho);//Herencia
         this.alto = alto;
-        this.peso =peso;
+        this.ancho =ancho;
     }
-    saludar(){
-        return `Hola! Soy un cuadrado. Mi color es ${this.color} y mis medidas son ${this.alto} x ${this.peso}`
-    }
-    areaCuadrado(a,b){
+    area(a,b){
         return a*b;
 
     }
@@ -68,18 +73,156 @@ class Circulo extends Figura{
     constructor(color){
         super(color);
     }
-    areaCuadrado(radio){
-        return  Math.PI*radio**2;
-       
+    area(radio){
+        const area1 =Math.PI*radio**2;
+        return  area1.toFixed(2)  
          
     }
 }
-circus = new Circulo("Oliva");
-console.log(circus.areaCuadrado(10))
 
-juanGuillermoCuadrado = new cuadradp("Bianconero",undefined,undefined);
-console.log(juanGuillermoCuadrado.saludar())
-console.log("mi area es "+juanGuillermoCuadrado.areaCuadrado(5,50))
+class Triangulo extends Figura{
+    constructor(color){
+        super(color)
+    }
+    area(base,altura){
+        const are =base*altura/2;
+        return are.toFixed(2);
+    }
+}
+class Trapecio extends Figura{
+    constructor(color){
+        super (color)
+    }
+    area(baseMenor,baseMayor,altura){
+        let sumBases=baseMenor+baseMayor;
+        let multiBaseAltura= sumBases*altura;
+        let area = multiBaseAltura/2;
+        return area;
+    }
+}
+
+trap= new Trapecio("verde")
+console.log(trap.area(10,15,14))
+
+tri= new Triangulo("Amarillo")
+console.log(tri.area(10,5))
+
+circus = new Circulo("Oliva");
+console.log(circus.area(10));
+
+juanGuillermoCuadrado = new Cuadradp("Bianconero",5,10);
+console.log("mi area es "+juanGuillermoCuadrado.area(50,50))
+
 
 piramide = new Figura("Rojo");
 console.log(piramide.color)
+
+/*
+
+class Persona{
+    constructor(nombre,apellido,edad,nacionalidad){//Atributos de la clase Persona
+        this.nombre = null;
+        this.edad = null;
+        this.apellido = null;
+        this.nacionalidad = null;
+
+    }
+    static quienEres(){ //Método estatico
+        console.log("Hay 8.010.359.063 personas en el mundo")
+    }
+    get getNombre(){
+        return this.nombre;
+       
+
+    }//Set solo puede alamcenar un parametro :c
+    set setNombre(nombre){
+        this.nombre= nombre;
+    }
+    get getApellido(){
+        return this.apellido;
+       
+
+    }//Set solo puede alamcenar un parametro :c
+    set setApellido(apellido){
+        this.apellido = apellido;
+        
+    }
+    get getEdad(){
+        return this.edad;
+       
+
+    }//Set solo puede alamcenar un parametro :c
+    set setEdad(edad){
+        this.edad = edad;
+        
+    }
+    get getNacionalidad(){
+        return this.nacionalidad;
+       
+
+    }//Set solo puede alamcenar un parametro :c
+    set setNacionalidad(nacionalidad){
+        this.nacionalidad = nacionalidad;
+        
+    }   
+
+}
+//Metodos setter y getter
+Gente = new Persona();
+Gente.setNombreApellido="pablo Santiago,Rojas";
+console.log(Gente.getNombreApellido);
+console.log(Gente.nombre)
+console.log(Gente.apellido)
+Gente.setApellido="Rojas";
+console.log(Gente.getApellido);
+Gente.setEdad=26;
+console.log(Gente.getEdad);
+Gente.setNacionalidad="Colombiano";
+console.log(Gente.getNacionalidad);
+
+
+//Metodo estatico
+Persona.quienEres()
+*/
+
+const carro = {
+    marca: 'Williams Racing',
+    modelo: 'FW45',
+    año: 2023,
+    color: 'Azul y Negro',
+    descripcion: function(){
+    let msj = `Hola! Mi marca es ${this.marca}, soy el modelo ${this.modelo} año ${this.año} y mi color es ${this.color}`;
+    
+    return msj;
+    }
+    }
+    
+    let ferrari = carro;
+    console.log(`Propiedad marca del objecto: ${carro.marca}`);
+    console.log(`Propiedad modelo del objecto: ${carro.modelo}`);
+    console.log(`Propiedad año del objecto: ${carro.año}`);
+    console.log(`Propiedad descripción del objecto: ${carro.descripcion()}`)
+
+
+const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+const [diaUno, diaDos, diaTres, diaCuatro, diaCinco, diaSeis, diaSiete] = diasSemana;
+
+console.log(diaUno);
+console.log(diaDos);
+console.log(diaTres);
+console.log(diaCuatro);
+console.log(diaCinco);
+console.log(diaSeis);
+console.log(diaSiete);
+
+
+const mascota = {
+    nombre : "Pedro",
+    edad : 5,
+    tamaño : "Grande",
+    color : "Blanco"
+    };
+    
+ const {nombre, edad, tamaño, color} = mascota;
+ console.log(edad);
