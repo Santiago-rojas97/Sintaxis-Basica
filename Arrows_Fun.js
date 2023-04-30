81ff368dbdf6c2cf5cace98756f3c2bc9ac0b495
santiago rojas <santiago_bogota@hotmail.com>
2023-04-30 11:46:36 -0500

    Funciones flecha

---
+++ Arrows_Fun.js
@@ -0,0 +1,19 @@
+const sumar = (num1,num2) => num1+num2;
+console.log(sumar(10,52));
+
+a => a+100;
+
+console.log()
+
+const compara = (a,b) => {
+    if (a<b){
+        return 1
+    }else {
+        return 0
+    }
+}
+console.log(compara(1,5))
+
+const compara2 = (a,b) => (a !==b && a % 2==0 ? 'par y menor' : 0)
+
+console.log(compara2(1,7))
