0fd9ec8d1b79626bb18c92094d1efbdc38560568
santiago rojas <santiago_bogota@hotmail.com>
2023-05-07 09:03:09 -0500

    SIntaxis jQuey y ejemplos

---
+++ jQuery/ejerc.js
@@ -0,0 +1,9 @@
+$(function(){
+
+console.log($("H1").html()) //Método que muesta el contenido HTML de un elemento
+//$("<h2>Libreria jQuery</h2>").insertAfter("H1") //Metodo que insa elementos HTML despues de un elemento. insertBefore() para insertar antes de un elemento.
+$("h2").addClass("subtitulo") //Metodo que agrega una clase a un elemento.
+
+
+
+})
