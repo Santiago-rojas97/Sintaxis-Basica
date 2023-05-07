da63f71a2cf52eec1a1a8f7fd7811b5a6bc1b6dc
santiago rojas <santiago_bogota@hotmail.com>
2023-05-07 14:30:47 -0500

    ..

--- Arrows_Fun.js
+++ Arrows_Fun.js
@@ -1,9 +1,6 @@
-const sumar = (num1,num2) => num1+num2;
+const sumar = (num1,num2) => num1-num2;
 console.log(sumar(10,52));
 
-a => a+100;
-
-console.log()
 
 const compara = (a,b) => {
     if (a<b){
@@ -14,6 +11,7 @@ const compara = (a,b) => {
 }
 console.log(compara(1,5))
 
-const compara2 = (a,b) => (a !==b && a % 2==0 ? 'par y menor' : 0)
+//Usando operador terniario (?) se valida una condicion y se ejecuta 'par y menor' o '0'
+const compara2 = (a,b) => (a !==b && a % 2==0 ? 'Par' : 'Impar')
 
-console.log(compara2(1,7))
+console.log(compara2(-9,50))
