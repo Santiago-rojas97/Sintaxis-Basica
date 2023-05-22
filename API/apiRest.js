c8d46a539db65ffc93a24e2af909592b86d7f749
santiago rojas <santiago_bogota@hotmail.com>
2023-05-22 08:31:50 -0500

    apiRest

--- API/apiRest.js
+++ API/apiRest.js
@@ -7,7 +7,7 @@ d.addEventListener("submit", (e) => {
     const $response=d.querySelector(".contact-form-response")
         $form=d.querySelector(".form")
 
-    fetch("https://formsubmit.co/ajax/llherrerap@correo.udistrital.edu.co",{
+    fetch("https://formsubmit.co/ajax/santiago_bogota@hotmail.com",{
         method:"POST",
         body: new FormData(e.target)
     })
